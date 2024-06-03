d7855e9cb7ed5a00d946e2778080e0dcb61a2d41
qomsad <149834513+qomsad@users.noreply.github.com>
2024-06-04 02:10:25 +0500

    sort

--- OshService/Domain/Specialty/SpecialtyService.cs
+++ OshService/Domain/Specialty/SpecialtyService.cs
@@ -37,7 +37,11 @@ public class SpecialtyService(SpecialtyRepository repository, IMapper mapper, Se
     public PageSearched<SpecialtyViewRead> Search(RequestPageSearch request)
     {
         var page = repository.Search(request,
-            query => repository.OrganizationScope(privilege.GetCurrentAdministratorOrganization(), query));
+            query =>
+            {
+                query = query.OrderBy(e => e.Created);
+                return repository.OrganizationScope(privilege.GetCurrentAdministratorOrganization(), query);
+            });
 
         return page.MapPageSearched(mapper.Map<IEnumerable<SpecialtyViewRead>>);
     }
