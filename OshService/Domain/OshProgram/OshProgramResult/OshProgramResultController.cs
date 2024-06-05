22d3b7ab44b77e93c82289a0f7c1ac88956c38e2
qomsad <149834513+qomsad@users.noreply.github.com>
2024-06-05 08:35:25 +0500

    results controller

---
+++ OshService/Domain/OshProgram/OshProgramResult/OshProgramResultController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Mime;
+using AspBoot.Data.Request;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OshService.Domain.User.User;
+
+namespace OshService.Domain.OshProgram.OshProgramResult;
+
+[ApiController]
+[Route("api/osh-program-result")]
+[Authorize(Roles = nameof(UserType.Admin))]
+[Produces(MediaTypeNames.Application.Json)]
+public class OshProgramResultController(OshProgramResultService service) : Controller
+{
+    [HttpGet]
+    public IActionResult GetResult([FromQuery] RequestPage request)
+    {
+        return Ok(service.GetResults(request));
+    }
+}
---
+++ OshService/Domain/OshProgram/OshProgramResult/OshProgramResultService.cs
@@ -0,0 +1,27 @@
+﻿using AspBoot.Data.Model;
+using AspBoot.Data.Request;
+using AspBoot.Service;
+using AutoMapper;
+using OshService.Domain.OshProgram.OshProgramAssignment;
+using OshService.Security;
+
+namespace OshService.Domain.OshProgram.OshProgramResult;
+
+[Service]
+public class OshProgramResultService(
+    OshProgramAssignmentRepository repository,
+    IMapper mapper,
+    SecurityService privilege)
+{
+    public object? GetResults(RequestPage request)
+    {
+        var page = repository.GetPaginated(request,
+            query =>
+            {
+                query = query.Where(e => e.Result != null);
+                query = query.OrderByDescending(e => e.AssignmentDate);
+                return repository.OrganizationScope(privilege.GetCurrentAdministratorOrganization(), query);
+            });
+        return page.MapPage(mapper.Map<IEnumerable<OshProgramAssignmentViewRead>>);
+    }
+}
